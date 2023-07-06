9e2be88a01674d93099a70baf374652d76efaded
Albin Antony <albin@example.com>
2023-07-06 12:20:39 +0530

    updated with s3

--- ECR/dev-ecr/backend.tf
+++ ECR/dev-ecr/backend.tf
@@ -10,3 +10,11 @@ terraform {
 provider "aws" {
   region  = "ap-south-1"
 }
+
+terraform {
+  backend "s3" {
+  bucket = "tf-script-al"
+  key = "dev/terraform.tfstate"
+  region = "ap-south-1"
+  }
+}
